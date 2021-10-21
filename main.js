1cd154bb519208aebde6ec5a5d2e0dc204cc6851
Minidinkde <77305261+Minidinkde@users.noreply.github.com>
2021-10-21 08:41:06 -0700

    Update main.js

--- main.js
+++ main.js
@@ -13479,7 +13479,7 @@ Game.Launch=function()
 		if (!Game.OnAscend)
 		{
 			
-			var unit=(Math.round(Game.cookiesd)==1?' cookie':' cookies');
+			var unit=(Math.round(Game.cookiesd)==1?' ice cream sandwich':' ice cream sandwiches');
 			var str=Beautify(Math.round(Game.cookiesd));
 			if (Game.cookiesd>=1000000)//dirty padding
 			{
